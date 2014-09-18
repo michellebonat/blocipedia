d509f30b98a111c49029b44842aa74e99299a8fd
michellebonat <michelle.bonat@gmail.com>
2014-09-17 18:01:36 -0700

    First commit and setup

---
+++ README.md
@@ -0,0 +1,9 @@
+== README
+
+This is a wikipedia-type app to teach web development and Rails4.
+
+It was built by Michelle Bonat with my awesome mentor John Sawers for the Bloc program.
+
+
+Please feel free to use a different markup language if you do not plan to run
+<tt>rake doc:app</tt>.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blocipedia::Application.config.secret_key_base = 'fc4270ad69fd621e4edc79d75a4190a7208969fe2166369302fc9b257d5bf5a6efa22778c0ab9d4ee10eb806b156cfa96981076a03f9ca22e20fb85d9c0c649f'

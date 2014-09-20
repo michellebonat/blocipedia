b3b3bd6dce43f5546390a00c94e6653cef56c1d9
michellebonat <michelle.bonat@gmail.com>
2014-09-20 16:36:15 -0700

    Better association naming

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -1,5 +1,5 @@
 class Wiki < ActiveRecord::Base
   belongs_to :user
   has_many :collaborations
-  has_many :users, through: :collaborations
+  has_many :collaborators, through: :collaborations, class_name: 'User'
 end

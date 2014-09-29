59af6becdbf1f8f964c5891f744a4a179dc2fda9
michellebonat <michelle.bonat@gmail.com>
2014-09-29 09:11:17 -0700

    Basic stripe setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Blocipedia::Application.routes.draw do
+  get "charges/create"
   get "users/owned_wikis"
   get "users/collaborated_wikis"
   resources :wikis
   #match '/wikis/edit_live/:wiki_id' => 'wikis#edit_live', :via => :put, :as => 'wikis_edit_live'
+  resources :charges, only: [:new, :create]
 
   get "welcome/index"
   get "welcome/about"

fa9f175f9951c52eda784345aafde7ab21667f1f
michellebonat <michelle.bonat@gmail.com>
2014-09-29 07:58:35 -0700

    Updated wiki policy for privacy and creation etc.

--- app/policies/wiki_policy.rb
+++ app/policies/wiki_policy.rb
@@ -1,4 +1,17 @@
 class WikiPolicy < ApplicationPolicy
+
+  def index?
+    true
+  end
+
+  def create?
+    user.present? && user.role?(:admin)
+  end
+
+  def update?
+    create?
+  end
+
   def show?
     record.public? || user.present?
   end

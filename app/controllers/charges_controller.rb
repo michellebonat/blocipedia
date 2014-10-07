73b890280963aa94cf80e626ea968b99da54337f
michellebonat <michelle.bonat@gmail.com>
2014-10-06 20:36:17 -0700

    Stripe debugging

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -5,17 +5,20 @@ class ChargesController < ApplicationController
   def new
     @stripe_btn_data = {
         key: "#{ Rails.configuration.stripe[:publishable_key] }",
-        description: "BigMoney Membership - #{current_user.name}",
+        description: "BigMoney Membership - #{current_user.email}",
         amount: 1_000
         # We're like the Snapchat for Wikipedia. But really.
     }
   end
 
   def create
-    @amount = params[:amount]
+    @amount = 1000
 
     # Creates a Stripe Customer object, for associating
     # with the charge
+
+    logger.info “Started Charges create”
+
     customer = Stripe::Customer.create(
         email: current_user.email,
         card: params[:stripeToken]
@@ -28,6 +31,8 @@ class ChargesController < ApplicationController
         description: "BigMoney Membership - #{current_user.email}",
         currency: 'usd'
     )
+
+    logger.info “Got response from Stripe charge inspect”
 
     current_user.update_attribute(:premium, true)
 
